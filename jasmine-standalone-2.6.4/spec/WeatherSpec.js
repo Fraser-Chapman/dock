5a672a890dafacf5c548b490efabed2f90d26f5d
Fraser-Chapman <fraserchapman16@gmail.com>
2018-10-08 15:10:49 +0100

    added weather can be stormy test and made it pass

---
+++ jasmine-standalone-2.6.4/spec/WeatherSpec.js
@@ -0,0 +1,14 @@
+describe("Weather", function (){
+    let weather;
+
+    beforeEach(function() {
+        weather = new Weather();
+    })
+
+    it("can be stormy", function(){
+
+        //spyOn(Math, 'random').and.returnValue(0);
+        expect(weather.isStormy()).toBe(true);
+    })
+
+});
