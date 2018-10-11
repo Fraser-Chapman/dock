e313c303ec76d7a7600a68940913c55fd3854f6f
Fraser-Chapman <fraserchapman16@gmail.com>
2018-10-11 10:19:26 +0100

    Lol i think i finished it

--- jasmine-standalone-2.6.4/spec/PortSpec.js
+++ jasmine-standalone-2.6.4/spec/PortSpec.js
@@ -1,13 +1,20 @@
 describe('Port', function(){
     let weather;
     let port;
+    let ship;
 
     beforeEach(function(){
         weather = new Weather();
         port = new Port(weather);
+        ship = new Ship(port);
     });
     it("has weather", function(){
         expect(port.getWeather()).toBe(weather)
     });
+    it("can add ships", function(){
+        port.addShip();
+
+        expect(port.getShips()).toContain(ship);
+    })
 
 });
--- jasmine-standalone-2.6.4/src/Port.js
+++ jasmine-standalone-2.6.4/src/Port.js
@@ -2,8 +2,15 @@ Port.prototype = {
     getWeather: function(){
         return this._weather;
     },
+    addShip: function(ship){
+        this._ships.push(ship);
+    },
+    getShips: function(){
+        return this._ships;
+    },
 };
 
 function Port(weather){
     this._weather = weather;
+    this._ships = [];
 };
